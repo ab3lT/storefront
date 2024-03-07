606155928076de65f24f178e9282e41b637995ee
Abel <se.abel.tadesse@gmail.com>
2024-03-08 00:28:57 +0300

    referencing fields  using q objects

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 from store.models import Product
 # Create your views here.
 # request -> response
@@ -7,7 +8,9 @@ from store.models import Product
 # action
 
 def hello(request):
-    query_set = Product.objects.filter(unit_price__range=(20,30))
+    # query_set = Product.objects.filter(unit_price__range=(20,30))
+    # query_set = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
+    query_set = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
     
     # for product in query_set:
     #     print(product)
