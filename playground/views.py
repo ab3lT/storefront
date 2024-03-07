d8933c897696bce105597587caaaa46c439c7653
Abel <se.abel.tadesse@gmail.com>
2024-03-07 09:16:11 +0300

    changing the bd to postgres

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 # Create your views here.
 # request -> response
 # request handler
 # action
 
 def hello(request):
-    x = 1
-    y = 2
-    # return HttpResponse("Hello world")
-    return render(request, 'hello.html', {'name': 'abel'})
+    query_set = Product.objects.filter(unit_price__range=(20,30))
+    
+    # for product in query_set:
+    #     print(product)
+    return render(request, 'hello.html', {'name': 'abel', 'products': list(query_set)})
--- store/models.py
+++ store/models.py
@@ -9,9 +9,10 @@ class Collection(models.Model):
     featured_product = models.ForeignKey('Product', on_delete = models.SET_NULL, null = True, related_name= '+')
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
     #9999/99
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now = True)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
@@ -47,6 +48,12 @@ class Customer(models.Model):
     birth_date = models.DateField(null = True)
     membership = models.CharField(max_length=1, choices = MEMBERSHIP_CHOICES, default = MEMBERSHIP_BRONZE)
     
+    class Meta:
+        db_table = 'store_customer'
+        indexes = [
+            models.Index(fields = ['first_name', 'last_name'])
+        ]
+    
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete = models.PROTECT)
     product = models.ForeignKey(Product, on_delete = models.PROTECT)
@@ -55,6 +62,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.PositiveSmallIntegerField()
     customer = models.OneToOneField(Customer, on_delete= models.CASCADE, primary_key = True)
 
 class Cart(models.Model):
