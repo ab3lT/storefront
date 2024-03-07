d5c3f0b66437e384f594c0dc0b057e922b177f10
Abel <se.abel.tadesse@gmail.com>
2024-03-08 00:39:02 +0300

    sorting data

--- playground/views.py
+++ playground/views.py
@@ -11,8 +11,14 @@ def hello(request):
     # query_set = Product.objects.filter(unit_price__range=(20,30))
     # query_set = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
     # query_set = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
-    query_set = Product.objects.filter(inventory= F('unit_price'))
+    # query_set = Product.objects.filter(inventory= F('unit_price'))
+    #sorting
+    #  query_set = Product.objects.order_by('unit_price','-title').reverse() 
+    # query_set = Product.objects.filter(collection__id=1).order_by('unit_price').reverse() 
+    # product = Product.objects.order_by('unit_price')[0] 
+    product = Product.objects.earliest('unit_price')
+    
     
     # for product in query_set:
     #     print(product)
-    return render(request, 'hello.html', {'name': 'abel', 'products': list(query_set)})
+    return render(request, 'hello.html', {'name': 'abel', 'product': product})
