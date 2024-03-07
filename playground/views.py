e04da2c4c7461f5fc2668eb372e6763fd0e3b0ff
Abel <se.abel.tadesse@gmail.com>
2024-03-08 00:31:37 +0300

    using F objects

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 # Create your views here.
 # request -> response
@@ -10,7 +10,8 @@ from store.models import Product
 def hello(request):
     # query_set = Product.objects.filter(unit_price__range=(20,30))
     # query_set = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
-    query_set = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    # query_set = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    query_set = Product.objects.filter(inventory= F('unit_price'))
     
     # for product in query_set:
     #     print(product)
