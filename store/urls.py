91b31b564db31f30c45a5a1eef45f8553e3eaaca
Abel <se.abel.tadesse@gmail.com>
2024-03-11 18:18:00 +0300

    serializing relationships by hyperlink

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,6 @@ from . import views
 #url conf module
 urlpatterns = [
     path('products/', views.product_list),
-    path('products/<int:id>/', views.product_detail)
+    path('products/<int:id>/', views.product_detail),
+    path('collection/<int:pk>/', views.collection_detail, name = 'collection-detail')
 ]
