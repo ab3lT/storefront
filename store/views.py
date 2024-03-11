3362ba577d1cc571454f3c73210dadc1601bb888
Abel <se.abel.tadesse@gmail.com>
2024-03-11 17:38:56 +0300

    one way of serializing and calling apis

--- store/views.py
+++ store/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 # Create your views here.
 
 @api_view()
@@ -10,4 +13,10 @@ def product_list(request):
 
 @api_view()
 def product_detail(request, id):
-    return Response(f'product {id}')
+    try : 
+        product = Product.objects.get(pk=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
